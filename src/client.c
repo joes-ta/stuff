8811532b99870af58d3cf17258cdfce5f170dbf8
jacoby <coffin.jacoby@icloud.com>
2022-04-19 21:26:41 -0400

    PLEASE

--- src/client.c
+++ src/client.c
@@ -3,121 +3,135 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 
-#pragma comment (lib, "Ws2_32.lib")
+#pragma comment(lib, "Ws2_32.lib")
 
-int main( void ) {
+int main(void)
+{
     WSADATA wsaData;
     SOCKET hostSock = INVALID_SOCKET;
     struct addrinfo *hostAddrInfo = NULL, *attemptAddrInfo = NULL, hintsAddrInfo;
-    char recvbuf[ 512 ];
+    char recvbuf[512];
     int recvbuflen = 512;
-    char *input= (char *)malloc(512);
+    char *input = (char *)malloc(512);
     int result;
-    char *quit="quit";
-    int ret;    
-        result = WSAStartup( MAKEWORD( 2,2 ), &wsaData );
-    
-    if ( result != 0 ) {
-        printf( "WSAStartup failed with error: %d\n", result );
+    char *quit = "quit";
+    int ret;
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if (result != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", result);
         return -1;
     }
 
-    ZeroMemory( &hintsAddrInfo, sizeof(hintsAddrInfo) ); //memset( &hintsAddrInfo, 0(intialize to 1),sizeof(hintsAddrInfo) )
-        hintsAddrInfo.ai_family = AF_UNSPEC;
-        hintsAddrInfo.ai_socktype = SOCK_STREAM;
-        hintsAddrInfo.ai_protocol = IPPROTO_TCP;
+    ZeroMemory(&hintsAddrInfo, sizeof(hintsAddrInfo));
+    hintsAddrInfo.ai_socktype = SOCK_STREAM;
+    hintsAddrInfo.ai_protocol = IPPROTO_TCP;
 
-        result = getaddrinfo( "localhost", "12345", &hintsAddrInfo, &hostAddrInfo );//Attempt to contact "localhost" at port 12345//List of address structures
-    
-    if ( result != 0 ) {
-        printf( "getaddrinfo failed with error: %d\n", result );
-        WSACleanup( );
+    result = getaddrinfo("localhost", "12345", &hintsAddrInfo, &hostAddrInfo);
+
+    if (result != 0)
+    {
+        printf("getaddrinfo failed with error: %d\n", result);
+        WSACleanup();
         return -2;
     }
 
-    for( attemptAddrInfo = hostAddrInfo; attemptAddrInfo != NULL ; attemptAddrInfo = attemptAddrInfo->ai_next )//ai_next=next address 
+    for (attemptAddrInfo = hostAddrInfo; attemptAddrInfo != NULL; attemptAddrInfo = attemptAddrInfo->ai_next)
     {
-        hostSock = socket( attemptAddrInfo->ai_family, attemptAddrInfo->ai_socktype, 
-            attemptAddrInfo->ai_protocol );
-        
-    if( hostSock == INVALID_SOCKET ) {
-            printf( "socket failed with error: %ld\n", WSAGetLastError( ) );
-            WSACleanup( );
-        return -3;
+        hostSock = socket(attemptAddrInfo->ai_family, attemptAddrInfo->ai_socktype,
+                          attemptAddrInfo->ai_protocol);
+
+        if (hostSock == INVALID_SOCKET)
+        {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return -3;
         }
 
-        result = connect( hostSock, attemptAddrInfo->ai_addr, (int)attemptAddrInfo->ai_addrlen); //-> member of
-    if( result == SOCKET_ERROR ) {
-            closesocket( hostSock );
-        hostSock = INVALID_SOCKET;
-        continue;
+        result = connect(hostSock, attemptAddrInfo->ai_addr, (int)attemptAddrInfo->ai_addrlen);
+        if (result == SOCKET_ERROR)
+        {
+            closesocket(hostSock);
+            hostSock = INVALID_SOCKET;
+            continue;
         }
         break;
     }
 
-            freeaddrinfo( hostAddrInfo );
+    freeaddrinfo(hostAddrInfo);
 
-    if( hostSock == INVALID_SOCKET ) {
-            printf( "Unable to connect to server!\n" );
-            WSACleanup( );
+    if (hostSock == INVALID_SOCKET)
+    {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
         return -4;
     }
-   do{
-            memset(input,0,512); //allocate 512 bit for input and store them as 0's
-            scanf("%511[^\n]",input); //scan through 511 bits for *c / message; looking for anything that is [^\n](Not a newline)  
-            scanf("%*c");//temporary place holder of current character.
-        ret=strcmp(quit,input);
-      switch (ret)
-      {
-          case 0:
-          closesocket( hostSock );
-            WSACleanup( );
+    do
+    {
+        memset(input, 0, 512);
+        scanf("%511[^\n]", input);
+        scanf("%*c");
+        ret = strcmp(quit, input);
+        switch (ret)
+        {
+        case 0:
+            closesocket(hostSock);
+            WSACleanup();
             return 0;
-          break;
-          case !0:
-      result = send( hostSock, input, (int)strlen(input)+1, 0 ); //(Socket,message,sizeof(message),flags)
-      break;
-      }
-       
-    if( result == SOCKET_ERROR ) {
-            printf( "send failed with error: %d\n", WSAGetLastError( ) );
-            closesocket( hostSock );
-            WSACleanup( );
-        return -5;
-    }
-            printf("Message Sent:%s\n", input);   
-            printf("Bytes Sent: %d\n", result);
-            printf("From Client\n\nPayload:\n");
-    int preview = 0;int total=0;
-    char communicate;
-     
-        result = recv( hostSock, recvbuf, recvbuflen, 0 );
-    if ( result > 0 ) {
-    	
-        total += result;
-	
-    if( preview < 1000 && ret!=0) {fwrite(recvbuf, 1, result, stdout); preview += result; } //Preview limits to 1000 characters printed.
-            
-            printf("\n\nThis Was From The Server\n\n");
-        } 
-    else if( result == 0 )
-            printf( "Connection Gets Closed Here\n" );    
-        
-    else
-            printf( "recv failed with error: %d\n", WSAGetLastError( ) );
+            break;
+        case !0:
+            result = send(hostSock, input, (int)strlen(input) + 1, 0);
+            break;
+        }
 
-    
-          //  printf( "Bytes Recieved: %ld\n", total);
-            //printf( "To Client\n");
-    }while(ret!=0);
-    result = shutdown( hostSock, SD_SEND );
-    if( result == SOCKET_ERROR ) {
-            printf( "shutdown failed with error: %d\n", WSAGetLastError( ) );
-            closesocket( hostSock );
-            WSACleanup( );
-            return -6;}
-        else    
-            closesocket( hostSock );
-            WSACleanup( );
-        return 0;
+        if (result == SOCKET_ERROR)
+        {
+            printf("send failed with error: %d\n", WSAGetLastError());
+            closesocket(hostSock);
+            WSACleanup();
+            return -5;
+        }
+        printf("Message Sent:%s\n", input);
+        printf("Bytes Sent: %d\n", result);
+        printf("From Client\n\nPayload:\n");
+        int preview = 0;
+        int total = 0;
+        char communicate;
+
+        result = recv(hostSock, recvbuf, recvbuflen, 0);
+        if (result > 0)
+        {
+
+            total += result;
+
+            if (preview < 1000 && ret != 0)
+            {
+                fwrite(recvbuf, 1, result, stdout);
+                preview += result;
+            }
+
+            printf("\n\nThis Was From The Server\n\n");
+        }
+        else if (result == 0)
+            printf("Connection Gets Closed Here\n");
+
+        else
+            printf("recv failed with error: %d\n", WSAGetLastError());
+
+        //printf( "Bytes Recieved: %ld\n", total);
+        //printf( "To Client\n");
+    } while (ret != 0);
+    result = shutdown(hostSock, SD_SEND);
+    if (result == SOCKET_ERROR)
+    {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(hostSock);
+        WSACleanup();
+        return -6;
+    }
+    else
+        closesocket(hostSock);
+    WSACleanup();
+    return 0;
 }
--- src/server.c
+++ src/server.c
@@ -7,34 +7,35 @@ struct player
 {
     char *playerid;
     char *inventory;
-        int health;
-        int wallet;
-        long int bank;
+    int health;
+    int wallet;
+    long int bank;
 };
 
-#pragma comment (lib, "Ws2_32.lib")
+#pragma comment(lib, "Ws2_32.lib")
 
-void check(int returninv,int returnhea,int returnwal,
-            char *inventory, int health,int wallet){
-    printf("bef:%d\n",returninv);
-                printf("%d\n",returnhea);
-                printf("%d\n",returnwal);
-    if( returninv == 0)
+void check(int returninv, int returnhea, int returnwal,
+           char *inventory, int health, int wallet)
+{
+    printf("bef:%d\n", returninv);
+    printf("%d\n", returnhea);
+    printf("%d\n", returnwal);
+    if (returninv == 0)
     {
-    printf("Your Inventory:%s\n",inventory);
+        printf("Your Inventory:%s\n", inventory);
     }
-    if( returnhea == 0)
+    if (returnhea == 0)
     {
-    printf("Your Health:%d\n",health);
+        printf("Your Health:%d\n", health);
     }
-    if( returnwal == 0)
+    if (returnwal == 0)
     {
-    printf("Your Wallet:$%d\n",wallet);
+        printf("Your Wallet:$%d\n", wallet);
     }
 };
 
-
-int main( void ) {
+int main(void)
+{
     WSADATA wsaData;
     SOCKET ListenSocket = INVALID_SOCKET;
     SOCKET ClientSocket = INVALID_SOCKET;
@@ -42,157 +43,170 @@ int main( void ) {
     //struct player *player,playerid;
     struct player player;
     int iSendResult;
-    char recvbuf[ 512 ];
+    char recvbuf[512];
     int recvbuflen = 512;
     int result;
-     int communicate=0;
-do{
-            result = WSAStartup( MAKEWORD(2,2), &wsaData );
-    
-        if( result != 0 ) {
-                printf( "WSAStartup failed with error: %d\n", result );
-            return -1;
-    }
+    int communicate = 0;
+    do
+    {
+        result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    ZeroMemory( &hintsAddrInfo, sizeof hintsAddrInfo );
+        if (result != 0)
+        {
+            printf("WSAStartup failed with error: %d\n", result);
+            return -1;
+        }
+
+        ZeroMemory(&hintsAddrInfo, sizeof hintsAddrInfo);
         hintsAddrInfo.ai_family = AF_INET;
         hintsAddrInfo.ai_socktype = SOCK_STREAM;
         hintsAddrInfo.ai_protocol = IPPROTO_TCP;
         hintsAddrInfo.ai_flags = AI_PASSIVE;
 
-        result = getaddrinfo( "localhost", "12345", &hintsAddrInfo, &hostAddrInfo );
-    
-            if ( result != 0 ) {
-                    printf( "getaddrinfo failed with error: %d\n", result );
-                    WSACleanup( );
-                return -2;
-    }
+        result = getaddrinfo("localhost", "12345", &hintsAddrInfo, &hostAddrInfo);
 
+        if (result != 0)
+        {
+            printf("getaddrinfo failed with error: %d\n", result);
+            WSACleanup();
+            return -2;
+        }
 
-        ListenSocket = socket( hostAddrInfo->ai_family, hostAddrInfo->ai_socktype, hostAddrInfo->ai_protocol );
-        
-            if( ListenSocket == INVALID_SOCKET ) {
-                    printf( "socket failed with error: %ld\n", WSAGetLastError( ) );
-                    freeaddrinfo( hostAddrInfo );
-                    WSACleanup( );
-                return -3;
-    }
+        ListenSocket = socket(hostAddrInfo->ai_family, hostAddrInfo->ai_socktype, hostAddrInfo->ai_protocol);
 
-        result = bind( ListenSocket, hostAddrInfo->ai_addr, (int)hostAddrInfo->ai_addrlen );
-    
-        if( result == SOCKET_ERROR ) {
-                printf( "bind failed with error: %d\n", WSAGetLastError( ) );
-                freeaddrinfo( hostAddrInfo );
-                closesocket( ListenSocket );
-                WSACleanup( );
+        if (ListenSocket == INVALID_SOCKET)
+        {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            freeaddrinfo(hostAddrInfo);
+            WSACleanup();
+            return -3;
+        }
+
+        result = bind(ListenSocket, hostAddrInfo->ai_addr, (int)hostAddrInfo->ai_addrlen);
+
+        if (result == SOCKET_ERROR)
+        {
+            printf("bind failed with error: %d\n", WSAGetLastError());
+            freeaddrinfo(hostAddrInfo);
+            closesocket(ListenSocket);
+            WSACleanup();
             return -4;
-    }
-                freeaddrinfo( hostAddrInfo );
+        }
+        freeaddrinfo(hostAddrInfo);
 
-            result = listen( ListenSocket, SOMAXCONN );
-        
-        if( result == SOCKET_ERROR ) {
-                printf( "listen failed with error: %d\n", WSAGetLastError( ) );
-                closesocket( ListenSocket );
-                WSACleanup( );
+        result = listen(ListenSocket, SOMAXCONN);
+
+        if (result == SOCKET_ERROR)
+        {
+            printf("listen failed with error: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
             return -5;
-    }
+        }
 
-            ClientSocket = accept( ListenSocket, NULL, NULL );
-        
-        if( ClientSocket == INVALID_SOCKET ) {
-                printf( "accept failed with error: %d\n", WSAGetLastError( ) );
-                closesocket( ListenSocket );
-                WSACleanup( );
+        ClientSocket = accept(ListenSocket, NULL, NULL);
+
+        if (ClientSocket == INVALID_SOCKET)
+        {
+            printf("accept failed with error: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
             return -6;
-    }
+        }
 
-                closesocket( ListenSocket );
-        
-       do { 
-            result = recv( ClientSocket, recvbuf, recvbuflen, 0 );
-            
-        if( result > 0 ) {
-        char *message;
-        int returninv;
-        int returnhea;
-        int returnwal;
-        int returnwrk;
-        int returnbnk;
-        char *inv="$inventory";
-        char *hea="$health";
-        char *wal="$wallet";
-        char *wrk="$work";
-        char *bnk="$bank";
-        int quitret;
-        player.inventory="A joe";
-        player.health=100;
-        player.wallet=1000;
-            message=(char *)malloc(512);
-                strcpy(message,recvbuf);
-                sscanf("%s",message); //Copies data of format string from variable (message)
+        closesocket(ListenSocket);
+
+        do
+        {
+            result = recv(ClientSocket, recvbuf, recvbuflen, 0);
+
+            if (result > 0)
+            {
+                char *message;
+                int returninv;
+                int returnhea;
+                int returnwal;
+                int returnwrk;
+                int returnbnk;
+                char *inv = "$inventory";
+                char *hea = "$health";
+                char *wal = "$wallet";
+                char *wrk = "$work";
+                char *bnk = "$bank";
+                int quitret;
+                player.inventory = "A joe";
+                player.health = 100;
+                player.wallet = 1000;
+                message = (char *)malloc(512);
+                strcpy(message, recvbuf);
+                sscanf("%s", message);
                 switch (recvbuf[0])
                 {
                 case '$':
-                returninv=strcmp(message, inv);
-                returnhea=strcmp(message, hea);
-                returnwal=strcmp(message, wal);
-                returnwrk=strcmp(message, wrk);
-                returnbnk=strcmp(message, bnk);
-                printf("invret:%d\n",returninv);
-                printf("hearet:%d\n",returnhea);
-                printf("walret:%d\n",returnwal);
-                printf("wrkret:%d\n",returnwrk);
-                printf("bnkret:%d\n",returnbnk);
-                check(returninv,returnhea,returnwal,
-                player.inventory,player.health,player.wallet);
-                break;
+                    returninv = strcmp(message, inv);
+                    returnhea = strcmp(message, hea);
+                    returnwal = strcmp(message, wal);
+                    returnwrk = strcmp(message, wrk);
+                    returnbnk = strcmp(message, bnk);
+                    printf("invret:%d\n", returninv);
+                    printf("hearet:%d\n", returnhea);
+                    printf("walret:%d\n", returnwal);
+                    printf("wrkret:%d\n", returnwrk);
+                    printf("bnkret:%d\n", returnbnk);
+                    check(returninv, returnhea, returnwal,
+                          player.inventory, player.health, player.wallet);
+                    break;
                 case '!':
-                break;
+                    break;
                 case 'q':
-                communicate=1;
+                    communicate = 1;
+                    closesocket(ClientSocket);
+                    WSACleanup();
+                    return 0;
+                    break;
+                default:
+                    printf("Error\n");
+                    break;
+                }
+                printf("Message received:%s\n", message);
+                //printf("\nFrom Client\n");
+                iSendResult = send(ClientSocket, recvbuf, result, 0);
+
+                if (iSendResult == SOCKET_ERROR)
+                {
+                    printf("send failed with error: %d\n", WSAGetLastError());
+                    closesocket(ClientSocket);
+                    WSACleanup();
+                    return -7;
+                }
+                printf("Bytes sent: %d\n", iSendResult);
+                printf("From Server\n");
+            }
+
+            else if (result == 0)
+                printf("Connection closing...Closing Client Socket\n");
+
+            else
+            {
+                printf("recv failed with error: %d\n", WSAGetLastError());
                 closesocket(ClientSocket);
                 WSACleanup();
-                return 0;
-                break;
-                default: printf("Error\n");
-                    break;}
-            printf("Message received:%s\n",message);
-            //printf("\nFrom Client\n");
-            iSendResult = send( ClientSocket, recvbuf, result, 0 );
-        
-        if( iSendResult == SOCKET_ERROR ) {
-                printf( "send failed with error: %d\n", WSAGetLastError( ) );
-                closesocket( ClientSocket );
-                WSACleanup( );
-            return -7;
+                return -8;
             }
-                printf( "Bytes sent: %d\n", iSendResult);
-                printf("From Server\n");
-        }
-        
-        else if( result == 0 )
-                printf( "Connection closing...Closing Client Socket\n" );
-        
-        else  {
-                printf( "recv failed with error: %d\n", WSAGetLastError( ) );
-                closesocket( ClientSocket );
-                WSACleanup( );
-            return -8;
-        }
 
-    }   while( result > 0 );
-            
-            result = shutdown( ClientSocket, SD_SEND );
-        if( result == SOCKET_ERROR ) {
-                printf( "shutdown failed with error: %d\n", WSAGetLastError( ) );
-                closesocket( ClientSocket );
-                WSACleanup( );
+        } while (result > 0);
+
+        result = shutdown(ClientSocket, SD_SEND);
+        if (result == SOCKET_ERROR)
+        {
+            printf("shutdown failed with error: %d\n", WSAGetLastError());
+            closesocket(ClientSocket);
+            WSACleanup();
             return -9;
-    }
+        }
 
-                closesocket( ClientSocket );
-                WSACleanup( );
-            return 0;
-}while (communicate=0);
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 0;
+    } while (communicate = 0);
 }
