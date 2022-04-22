67487febd8cc1e3b0bd208a8190da56d102b14fd
jpatrick5402 <jpatrick5402@gmail.com>
2022-04-22 12:52:23 -0400

    I forgot to Save

--- src/savestate.c
+++ src/savestate.c
@@ -2,3 +2,21 @@
 
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::vector;
+
+int main()
+{
+	string username="jpatric5"
+	string filename("input.txt");
+	vector<string> lines;
+	string line;
+	
+	ifstream input_file(username)
+	
+}
