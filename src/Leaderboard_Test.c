81986cadcf655f1f4bb05ee6d664e38d4616538d
Gavin Suber <gavinsuber13@gmail.com>
2022-04-28 21:00:09 -0400

    Updated Leaderboard_Test.c to use size of array, rather than size of address

--- src/Leaderboard_Test.c
+++ src/Leaderboard_Test.c
@@ -14,8 +14,8 @@ struct player
 typedef struct player Player;
 
 //In main function, create int returnldr and also add returnldr in the see function (displays the appropriate players data)
-void Action(int returnLeaderboard, int arrSize, Player * players)
-{    
+void Action(int returnLeaderboard, int arrSize, Player* players)
+{
     if (returnLeaderboard == 0)
     {
         //Temporarily stores the smaller of the two values checked, so it can be stored in i+1
@@ -61,20 +61,21 @@ int main()
 {
     const int playerAmount = 15;
     int long PlayerBankData[15] = { 100, 30, 500, 50, 300, 29, 1001, 3883, 23432, 123, 100, 0, 3, 2, 4 };
-    const char* Name[15] = {"Test1", "Test2", "Test3", "Test4", "Test5", "Test6", "Test7", "Test8","Test9", "Test10", "Test11", "Test12", "Test13", "Test14", "Test15"};
-        
+    const char* Name[15] = { "Test1", "Test2", "Test3", "Test4", "Test5", "Test6", "Test7", "Test8","Test9", "Test10", "Test11", "Test12", "Test13", "Test14", "Test15" };
+
     struct player game_Players[15];
 
+    int NameSize = sizeof(Name) / sizeof(Name[0]);
     //Populates game_Players structs with stored information
     for (int a = 0; a < playerAmount; a++)
     {
         game_Players[a].playerid = a;
-        
-        strcpy_s(game_Players[a].Name, sizeof(Name[a]), Name[a]);
+
+        strcpy_s(game_Players[a].Name, NameSize, Name[a]);
 
         game_Players[a].bank = PlayerBankData[a];
     }
-
+    //printf("%d", NameSize);
     int return_Leaderboard = 0;
     Action(return_Leaderboard, playerAmount, game_Players);
 }
